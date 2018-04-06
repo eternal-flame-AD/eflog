d27133d113b0051f8ffd335174cae9e3bee7f037
_eternal_flame <35571479+eternal-flame-AD@users.noreply.github.com>
2018-04-06 18:23:18 +0800

    try fix python 2.7(attempt 2)

--- eflog.py
+++ eflog.py
@@ -7,8 +7,7 @@ except ImportError:
     '''
     import os
     common_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    +"/common/"
-    sys.path.insert(0, common_dir)
+    sys.path.insert(0, common_dir+"/common/")
     import loghandler
 
 
